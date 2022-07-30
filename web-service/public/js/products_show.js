700462230f7a314e3c9fecb7950c91e78aa3f080
Reza <effati78@protonmail.com>
2022-07-30 11:12:54 +0430

    Added ability to delete product and add comment

--- web-service/public/js/products_show.js
+++ web-service/public/js/products_show.js
@@ -1,27 +1,41 @@
-let product_cnt = $('.product_cnt');
-let product_api = '';
+let product_cnt = $(".product_cnt");
+let product_api = "";
 let id = window.location.href.slice(31);
-fetch(`http://127.0.0.1:8000/api/products/${id}`)
-    .then(res => res.json())
-    .then(function(json) {
+
+const comment_url = `http://127.0.0.1:8000/api/comments/${id}`;
+const product_url = `http://127.0.0.1:8000/api/products/${id}`;
+
+fetch(product_url)
+    .then((res) => res.json())
+    .then(function (json) {
         product_api = `<div class="col-12 row p-2 bg-white border rounded">
                             <div class="col-md-3"><img class="img-fluid img-responsive rounded product-image"
                                     src="${json.product.image_url}"></div>
                             <div class="col-md-6 mt-1">
-                                <h5 class="font-weight-bold mb-3">${json.product.title}</h5>
-                                <p class="text-justify para mt-2 font-md pre">${json.product.content}</p>
+                                <h5 class="font-weight-bold mb-3">${
+                                    json.product.title
+                                }</h5>
+                                <p class="text-justify para mt-2 font-md pre">${
+                                    json.product.content
+                                }</p>
                             </div>
                             <div class="align-items-center align-content-center col-md-3 border-right mt-1">
                                 <div class="d-flex flex-row align-items-center justify-content-center">
-                                    <h4 class="mr-1 text-center d-block mb-4 price">${json.product.price}</h4>
+                                    <h4 class="mr-1 text-center d-block mb-4 price">${
+                                        json.product.price
+                                    }</h4>
                                 </div>
-                                <h6 class="${json.product.isFeatured == 0 ? 'd-none' : ''} text-white bg-success p-2 text-center"><i class="far fa-gift"></i> محصول ویژه</h6>
-                                <h6 class="bg-info text-white p-2 mt-2 text-center"><i class="far fa-star"></i> امتیاز محصول ${json.product.score}</h6>
+                                <h6 class="${
+                                    json.product.isFeatured == 0 ? "d-none" : ""
+                                } text-white bg-success p-2 text-center"><i class="far fa-gift"></i> محصول ویژه</h6>
+                                <h6 class="bg-info text-white p-2 mt-2 text-center"><i class="far fa-star"></i> امتیاز محصول ${
+                                    json.product.score
+                                }</h6>
                                 
                                 <hr />
                                 <div class="d-flex align-items-center justify-content-between admin-btnS">
-                                    <button class="btn btn-sm btn-warning" title="ویرایش"><i class="far fa-edit"></i> ویرایش</button>
-                                    <button class="btn btn-sm btn-danger" title="حذف"><i class="far fa-trash-alt"></i> حذف</button>
+                                    <button class="btn btn-sm btn-warning" title="ویرایش" data-toggle="modal" data-target="#edit_prd"><i class="far fa-edit"></i> ویرایش</button>
+                                    <button class="btn btn-sm btn-danger delete_prd" title="حذف"><i class="far fa-trash-alt"></i> حذف</button>
                                 </div>
                             </div>
                         </div>`;
@@ -30,9 +44,12 @@ fetch(`http://127.0.0.1:8000/api/products/${id}`)
 
 let comments_cnt = $(".comments_cnt");
 let comments_api = "";
-fetch(`http://localhost:8000/api/comments/${id}`)
+
+fetch(comment_url)
     .then((res) => res.json())
     .then(function (json) {
+        if (json.comments.length > 0) $(".comments-registered").show();
+
         for (let i = 0; i < json.comments.length; i++) {
             comments_api += `<div class="col-12 row p-2 bg-white border rounded">
                                 <div class="avatar"><img class="img-fluid img-responsive rounded product-image"
@@ -53,3 +70,65 @@ fetch(`http://localhost:8000/api/comments/${id}`)
         }
         comments_cnt.html(comments_api);
     });
+
+$(document).on("click", ".delete_prd", function (e) {
+    e.preventDefault();
+
+    Swal.fire({
+        title: "محصول مورد نظر حذف شود؟",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "حذف کن",
+        cancelButtonText: "لغو",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(product_url, { method: "DELETE" })
+                .then((res) => res.json())
+                .then();
+
+            Swal.fire({
+                icon: "success",
+                title: "موفق!",
+                text: "محصول با موفقیت حذف شد.",
+                showConfirmButton: false,
+                timer: 5000,
+                timerProgressBar: true,
+                willClose: () => {
+                    window.location = "http://127.0.0.1:8000/";
+                },
+            });
+        }
+    });
+});
+
+$(".post-comment").submit(function (e) {
+    e.preventDefault();
+
+    fetch(comment_url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            name: $("#name").val(),
+            email: $("#email").val(),
+            avatar: $("#avatar").val(),
+            score: $("#score").val(),
+            comment: $("#comment").val(),
+        }),
+    })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+
+    Swal.fire({
+        icon: "success",
+        title: "موفق!",
+        text: "نظر با موفقیت افزوده شد.",
+        showConfirmButton: false,
+        timer: 5000,
+        timerProgressBar: true,
+        willClose: () => {
+            location.reload();
+        },
+    });
+});
--- web-service/resources/views/products_show.blade.php
+++ web-service/resources/views/products_show.blade.php
@@ -12,41 +12,101 @@
     <hr class="my-5">
 
     <h5 class="mb-1 text-center">لطفا نظر خود را برای ما به اشتراک بگذارید</h5>
-    <p class="mb-5 text-center font-weight-light">نظر شما ارزشمند است</ح>
-    <form class="row mb-5 px-0">
+    <p class="mb-5 text-center font-weight-light">نظر شما ارزشمند است</p>
+    <form class="row mb-5 px-0 post-comment" action="javascript:void(0)" method="POST">
         <div class="form-group col-6">
-            <label for="exampleFormControlInput1">پروفایل:</label>
-            <input type="file" accept="image/*" class="form-control" placeholder="عکس پروفایل خود را وارد کنید">
+            <label>پروفایل:</label>
+            <input name="avatar" id="avatar" type="file" accept="image/*" class="form-control"
+                placeholder="عکس پروفایل خود را وارد کنید">
         </div>
         <div class="form-group col-6">
-            <label for="exampleFormControlInput1">نام و نام‌خانوادگی:</label>
-            <input type="text" class="form-control" placeholder="نام و نام‌خانوادگی خود را وارد کنید">
+            <label>نام و نام‌خانوادگی:</label>
+            <input name="name" id="name" type="text" class="form-control"
+                placeholder="نام و نام‌خانوادگی خود را وارد کنید">
         </div>
         <div class="form-group col-6">
-            <label for="exampleFormControlInput1">ایمیل:</label>
-            <input type="email" class="form-control" placeholder="name@example.com">
+            <label>ایمیل:</label>
+            <input name="email" id="email" type="email" class="form-control" placeholder="name@example.com">
         </div>
         <div class="form-group col-6">
-            <label for="exampleFormControlSelect1">امتیاز شما به محصول:</label>
-            <select class="form-control">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+            <label>امتیاز شما به محصول:</label>
+            <select name="score" id="score" class="form-control">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
             </select>
         </div>
         <div class="form-group col-12">
             <label for="exampleFormControlTextarea1">نظر شما:</label>
-            <textarea class="form-control" placeholder="نظر خود را در مورد این محصول برای ما به اشتراک بگذارید" rows="3"></textarea>
+            <textarea name="comment" id="comment" class="form-control"
+                placeholder="نظر خود را در مورد این محصول برای ما به اشتراک بگذارید" rows="3"></textarea>
         </div>
         <div class="col-12 d-flex align-items-center justify-content-end">
             <button type="submit" class="btn btn-primary">ثبت نظر</button>
         </div>
     </form>
 
-    <h6 class="mb-3 text-right">نظرات ثبت شده</h6>
+    <h6 class="mb-3 text-right comments-registered">نظرات ثبت شده</h6>
     <div class="w-100 row mx-0 gap-15 comments_cnt"></div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="edit_prd" tabindex="-1" role="dialog" aria-labelledby="edit_prdTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header d-flex align-items-center justify-content-between">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close m-0 p-0" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="row mb-5 px-0 post-comment" action="javascript:void(0)" method="POST">
+                        <div class="form-group col-6">
+                            <label>عکس محصول:</label>
+                            <input name="image" id="prd_image" type="file" accept="image/*" class="form-control"
+                                placeholder="عکس محصول را وارد کنید">
+                        </div>
+                        <div class="form-group col-6">
+                            <label>نام محصول:</label>
+                            <input name="title" id="prd_title" type="text" class="form-control"
+                                placeholder="نام محصول را وارد کنید">
+                        </div>
+                        <div class="form-group col-6">
+                            <label>ایمیل:</label>
+                            <input name="email" id="email" type="email" class="form-control"
+                                placeholder="name@example.com">
+                        </div>
+                        <div class="form-group col-6">
+                            <label>امتیاز شما به محصول:</label>
+                            <select name="score" id="score" class="form-control">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-12">
+                            <label for="exampleFormControlTextarea1">نظر شما:</label>
+                            <textarea name="comment" id="comment" class="form-control"
+                                placeholder="نظر خود را در مورد این محصول برای ما به اشتراک بگذارید" rows="3"></textarea>
+                        </div>
+                        <div class="col-12 d-flex align-items-center justify-content-end">
+                            <button type="submit" class="btn btn-primary">ثبت نظر</button>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('site_js_files')
